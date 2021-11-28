6c4e923bdc1c4ccb851789d9eef1e1bb17a1a5e6
programmerraja <boooathis123@gmail.com>
2021-11-28 14:26:07 +0530

    logo added

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -61,7 +61,7 @@ function Nav({user}) {
 return ( <div className="nav">
               <div className="nav-brand">
                     <Link to="/">  
-                      Yuki22 
+                        <img src={logo}/>                      
                     </Link>
               </div>
 
