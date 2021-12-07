aeaf9adea1e0b2839bb3ff686675a58b7522c2c1
programmerraja <boooathis123@gmail.com>
2021-12-07 15:28:03 +0530

    alert added if new user visit home page

--- util/util.js
+++ util/util.js
@@ -9,12 +9,13 @@ function sendWhoIs(req){
     let browser = useragent["browser"]["name"];
     let os = useragent["os"]["name"];
     let device = useragent["device"];
-    sendReport(`new user visting home page from \n\n 
+    sendReport(`new user visting home page from  \n\n 
     				ip: ${ip} \n\n 
     				useragent:${JSON.stringify(useragent)} \n\n
     				browser:${browser} \n\n 
     				os:${os} \n\n 
     				device:${JSON.stringify(device)} 
+    				path:${req.path}
     		`);
 }
 
