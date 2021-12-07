d3d87963309abc2114b679046f4fbce46cab9180
programmerraja <boooathis123@gmail.com>
2021-12-07 15:40:40 +0530

    alert added if new user visit home page

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get("/", (req, res) => {
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  sendWhoIs(req);
+  // sendWhoIs(req);
 });
 
 app.listen(PORT, () => {
