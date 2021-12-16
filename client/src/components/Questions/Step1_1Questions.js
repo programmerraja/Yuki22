4043cedae31cabdb943fab4ed013079bad077757
K.Boopathi <44333589+programmerraja@users.noreply.github.com>
2021-12-16 06:20:46 +0530

    Step1_1 questions placeholder updated

--- client/src/components/Questions/Step1_1Questions.js
+++ client/src/components/Questions/Step1_1Questions.js
@@ -9,9 +9,9 @@ function Step1_1Questions({off_campus_detail,setOffCampusDetail}) {
                   <div  className="add_review-from">
                  
                    <label for="off_campus_detail" className="add_review-label">
-                   <span>Plse Explain how you get the opportunity for the off campus? <span className="red_color">*</span></span></label>
+                   <span>Plse explain how you get the opportunity for the off campus? <span className="red_color">*</span></span></label>
                           <div className="add_review-input-wrapper">
-                              <textarea placeholder="write full details about the round" 
+                              <textarea placeholder="example: Applied through LinkedIn/applied through pool campus drive " 
                                     rows="7"
                                     cols="24" 
                                     name="off_campus_detail"  
