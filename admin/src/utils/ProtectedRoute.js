c18d93286f2405c81a83b8cc5ca7a5c13e5fd82b
programmerraja <boooathis123@gmail.com>
2021-12-23 09:51:23 +0530

    admin user search functionality added

--- admin/src/utils/ProtectedRoute.js
+++ admin/src/utils/ProtectedRoute.js
@@ -3,15 +3,15 @@ import { Route, Redirect } from "react-router-dom";
 import API from "./API";
 
 function ProtectedRoute({ path, component: Component }) {
+	
 	return(<Route exact
 				path={path}
 				render={(props) => (	
-				true? 
+				API.isAuth() && !API.checkTokenExp()? 
 				<Component  /> :
 				<Redirect to='/signin' />
 				)}
 				/>);
 }
-// API.isAuth() && !API.checkTokenExp()
 
 export default ProtectedRoute;
