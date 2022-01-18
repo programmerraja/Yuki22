5c8db93764a8ea39255290fe6622998e4a43cb72
programmerraja <boooathis123@gmail.com>
2022-01-18 11:21:43 +0530

    UI updated for admin

--- models/index.js
+++ models/index.js
@@ -7,15 +7,15 @@ const fs = require("fs");
 //for back up the data
 
 // User.find({}).then((u)=>{
-// fs.writeFile("./user.json",JSON.stringify(u),"utf-8",(e)=>{console.log(e,"saved")})  
+// fs.writeFile("../backup/user.json",JSON.stringify(u),"utf-8",(e)=>{console.log(e,"saved")})  
 // })
 
 // Compaines.find({}).then((u)=>{
-// fs.writeFile("./company.json",JSON.stringify(u),"utf-8",(e)=>{console.log(e,"saved")})  
+// fs.writeFile("../backup/company.json",JSON.stringify(u),"utf-8",(e)=>{console.log(e,"saved")})  
 // })
 
 // Reviews.find({}).then((u)=>{
-// fs.writeFile("./reviews.json",JSON.stringify(u),"utf-8",(e)=>{console.log(e,"saved")})  
+// fs.writeFile("../backup/reviews.json",JSON.stringify(u),"utf-8",(e)=>{console.log(e,"saved")})  
 // })
 
 
